1e8164fc742495df6cd42c8da0b6976b1d3658a1
projetsJBL <projetsjbl@gmail.com>
2016-12-13 20:45:32 -0500

    Update GameEvents.java

--- GameEvents.java
+++ GameEvents.java
@@ -1,5 +1,6 @@
 package fr.mrmephisto.game1;
 
+import fr.mrmephisto.game1.bombs.BasicBomb;
 import fr.mrmephisto.game1.entities.Player;
 import fr.mrmephisto.game1.gfx.Colour;
 import fr.mrmephisto.game1.gfx.MyFont;
@@ -10,30 +11,61 @@ public class GameEvents {
 	static long lastTime;
 	private int playerHealth = 2;
 	static boolean playerDead = false;
-	static int bullets = 0;
-	public static int shotbullet = 0;
-	
+
+	public static int bombs = 10;
+
 	public GameEvents() {
-		
+
 	}
 
-	public void renderInterface(Screen screen, int x, int y) {		
-			if(playerHealth == 2){
-				//MyFont.render("c", screen, 50, 50, Colour.get(-1, 550, 500, -1), 2);
-				MyFont.render("c", screen, x, y, Colour.get(-1, 550, 500, -1), 1);
+	public void renderInterface(Screen screen, int x, int y) {
+		if (!playerDead) {
+			switch (playerHealth) {
+			case 2:
+				MyFont.render("c", screen, x, y, Colour.get(-1, 550, 500, -1),
+						1);
+				break;
+			case 1:
+				MyFont.render("h", screen, x, y, Colour.get(-1, 550, 500, -1),
+						1);
+				break;
+			case 0:
+				MyFont.render("n", screen, x, y, Colour.get(-1, 550, 500, -1),
+						1);
+				break;
 			}
-			/*
-			switch(playerHealth){
-			case 3:
-				int colour = Colour.get(-1, 550, 500, -1);
-				MyFont.render("ccc", screen, x, y, colour, 1);
-			}
-			*/
 		}
-	//}
-	
-	public void renderEvents(Screen screen, int x, int y, InputHandler input, Player player, Level level){
-		
+	}
+
+	public void renderEvents(Screen screen, int x, int y, InputHandler input,
+			Player player, Level level) {
+		if (input.space.isPressed() && !player.isSwimming && bombs > 0) {
+			BasicBomb b = new BasicBomb(level, player.x, player.y);
+			level.addBasicBomb(b);
+			System.out.println(bombs);
+		}
+
+		// getting damages from bombs
+		for (BasicBomb b : level.bombs) {
+			if (b.isExploding() && (player.x == b.x || player.y == b.y)
+					&& playerHealth > 0) {
+				player.gettingDamage = true;
+				if (System.currentTimeMillis() >= lastTime) {
+					lastTime = System.currentTimeMillis() + 1000;
+					playerHealth--;
+					if (playerHealth <= 0)
+						playerDead = true;
+				} else {
+					Player.gettingDamage = false;
+				}
+			}
+		}
+
+		if (playerDead) {
+			Game.level = new Level("/game_over.png");
+			MyFont.render("GAME OVER", screen, player.x - screen.width/2,
+					player.y, Colour.get(-1, -1, -1, 555), 1);
+		}
 	}
 
 }
